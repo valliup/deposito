45a3876f78533848d81311bdb7b0cf543a28bf16
detor <detor@DESKTOP-AQ3E86G>
2023-02-25 11:00:14 +0100

    Realizacion de commit

---
+++ src/cuentas/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuentas;
+
+/**
+ *
+ * @author detor
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        float cantidad = 0;
+
+        operativa_cuenta(cantidad);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
